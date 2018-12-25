5939819cfaeb8af3f0fc32d302972c267ba1e155
Konrad <konrad.trade@gmail.com>
2018-12-25 01:45:59 +0100

    First shemas and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+import { GraphQLServer } from "graphql-yoga";
+
+// Schemas
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+// Resolvers
+
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is a simple hello message!";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});
