292e091257a04e6f227ef3ef30c49e25bd371f4c
Konrad <konrad.trade@gmail.com>
2018-12-28 00:43:04 +0100

    Finished deleting comments and posts

--- src/index.js
+++ src/index.js
@@ -81,7 +81,9 @@ const typeDefs = `
         createUser(data: CreateUserInput!): User!
         deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
+        deletePost(id: ID!): Post!
         createComment(data: CreateCommentInput!): Comment!
+        deleteComment(id: ID!): Comment!
     }
     input CreateUserInput {
         name: String!
@@ -204,6 +206,18 @@ const resolvers = {
       posts.push(post);
       return post;
     },
+    deletePost(parent, args, ctx, info) {
+      const indexPost = posts.findIndex(post => post.id === args.id);
+
+      if (indexPost === -1) {
+        throw new Error("Such post doesn't exists.");
+      }
+      const deletedPost = posts.splice(indexPost, 1);
+
+      comments = comments.filter(comment => comment.post !== args.id);
+
+      return deletedPost[0];
+    },
     createComment(parent, args, ctx, info) {
       const isPostExisting = posts.some(post => post.id === args.data.post);
       const doesAuthorExists = users.some(user => user.id === args.data.author);
@@ -220,6 +234,18 @@ const resolvers = {
       comments.push(comment);
 
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(
+        comment => comment.id === args.id
+      );
+
+      if (commentIndex === -1) {
+        throw new Error("Comment doesn't exist");
+      }
+      const deletedComments = comments.splice(commentIndex, 1);
+
+      return deletedComments[0];
     }
   },
   Post: {
