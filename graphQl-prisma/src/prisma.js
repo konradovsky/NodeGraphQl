58e662926fdd60f6063497f9f23e30e994ab9d13
Konrad <konrad.trade@gmail.com>
2019-01-04 15:35:08 +0100

    Shrink prisma bindings

--- graphQl-prisma/src/prisma.js
+++ graphQl-prisma/src/prisma.js
@@ -22,27 +22,24 @@ const createPostForUser = async (authorId, data) => {
         }
       }
     },
-    "{id}"
+    "{ author {id name email posts {id title published }}}"
   );
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: authorId
-      }
-    },
-    "{ id name email posts {id title published }}"
-  );
-  return user;
+  return post.author;
 };
-createPostForUser("cjqh87l9q002a0770cjmu4liv", {
-  title: "This i a title for the new 2d2post",
-  body: "THERE you have a new body for the one and best",
-  published: true
-})
-  .then(user => console.log(JSON.stringify(user, undefined, 2)))
-  .catch(error => console.log(error));
+
+// createPostForUser("cjqh87l9q002a0770cjmu4liv", {
+//   title: "Hhahha",
+//   body: "THERE you have a new body for the one and best",
+//   published: true
+// })
+//   .then(user => console.log(JSON.stringify(user, undefined, 2)))
+//   .catch(error => console.log(error.message));
 
 const updatePostForUser = async (postId, data) => {
+  const postExists = prisma.exists.Post({ id: postId });
+  if (!postExists) {
+    throw new Error("Post not found");
+  }
   const post = await prisma.mutation.updatePost(
     {
       data: {
@@ -52,19 +49,15 @@ const updatePostForUser = async (postId, data) => {
         id: postId
       }
     },
-    "{author {id}}"
+    "{author {id name email posts {id title published }}}"
   );
-  const user = await prisma.query.user(
-    {
-      where: {
-        id: post.author.id
-      }
-    },
-    "{ id name email posts {id title published }}"
-  );
-  return user;
+  return post.author;
 };
 
-// updatePostForUser("cjqhq8zxz002s0770hkjiqeyi", {
-//   title: "Fuck yeah"
-// }).then(data => console.log(JSON.stringify(data, undefined, 2)));
+updatePostForUser("cjqhq8zxz002s0770hkjiqeyi", {
+  title: "Fuck yeah v3"
+})
+  .then(data => console.log(JSON.stringify(data, undefined, 2)))
+  .catch(error => {
+    console.log(error.message);
+  });
