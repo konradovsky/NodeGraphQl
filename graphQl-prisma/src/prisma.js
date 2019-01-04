be215a22ccbf66245e3ebe98db646f0901980434
Konrad <konrad.trade@gmail.com>
2019-01-04 11:48:16 +0100

    Added async await and error handling

--- graphQl-prisma/src/prisma.js
+++ graphQl-prisma/src/prisma.js
@@ -5,6 +5,66 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466"
 });
 
-prisma.query.users(null, "{id name email}").then(data => {
-  console.log(data);
-});
+const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({ id: authorId });
+
+  if (!userExists) {
+    throw new Error("User not found");
+  }
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId
+          }
+        }
+      }
+    },
+    "{id}"
+  );
+  const user = await prisma.query.user(
+    {
+      where: {
+        id: authorId
+      }
+    },
+    "{ id name email posts {id title published }}"
+  );
+  return user;
+};
+createPostForUser("cjqh87l9q002a0770cjmu4liv", {
+  title: "This i a title for the new 2d2post",
+  body: "THERE you have a new body for the one and best",
+  published: true
+})
+  .then(user => console.log(JSON.stringify(user, undefined, 2)))
+  .catch(error => console.log(error));
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost(
+    {
+      data: {
+        ...data
+      },
+      where: {
+        id: postId
+      }
+    },
+    "{author {id}}"
+  );
+  const user = await prisma.query.user(
+    {
+      where: {
+        id: post.author.id
+      }
+    },
+    "{ id name email posts {id title published }}"
+  );
+  return user;
+};
+
+// updatePostForUser("cjqhq8zxz002s0770hkjiqeyi", {
+//   title: "Fuck yeah"
+// }).then(data => console.log(JSON.stringify(data, undefined, 2)));
