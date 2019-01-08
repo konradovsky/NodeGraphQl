621f401f1c82ed54652be472d3a89e9b8a973cb2
Konrad <konrad.trade@gmail.com>
2019-01-09 00:24:19 +0100

    Support for password field

--- graphQl-prisma/src/prisma.js
+++ graphQl-prisma/src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://localhost:4466/prisma"
+  endpoint: "http://localhost:4466/prisma",
+  secret: "thisismysupersecretpass"
 });
 
 export { prisma as default };
